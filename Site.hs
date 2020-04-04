7e303ce455c9915d3fb9a7a0e9bddb76f3c25145
Rob Rix <rob.rix@me.com>
2020-04-04 19:12:54 -0400

    Add a hakyll package.

---
+++ Site.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main
+( main
+) where
+
+import Data.Monoid (mappend)
+import Hakyll
+
+main :: IO ()
+main = hakyll $ do
+  match "images/*" $ do
+    route   idRoute
+    compile copyFileCompiler
+
+  match "css/*" $ do
+    route   idRoute
+    compile compressCssCompiler
+
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "_layouts/post.html"    postCtx
+      >>= loadAndApplyTemplate "_layouts/default.html" postCtx
+      >>= relativizeUrls
+
+
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let indexCtx
+            =  listField "posts" postCtx (return posts)
+            <> defaultContext
+
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "_layouts/default.html" indexCtx
+        >>= relativizeUrls
+
+  match "_layouts/*" $ compile templateBodyCompiler
+
+postCtx :: Context String
+postCtx
+  =  dateField "date" "%B %e, %Y"
+  <> defaultContext
