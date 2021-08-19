5f1c4c0bfa07e1948141282916b28fc36c578960
Rob Rix <rob.rix@me.com>
2021-08-19 00:03:21 -0400

    Define sum.

--- docs/js/polarized.js
+++ docs/js/polarized.js
@@ -1,5 +1,6 @@
 import { component } from './sequent.js';
 
 export const SeqWith = component('seq-with')`<seq-op name="&amp;" neg><slot></slot></seq-op>`;
+export const SeqSum = component('seq-sum')`<seq-op name="⊕" pos><slot></slot></seq-op>`;
 export const SeqNot = component('seq-not')`<seq-op name="¬" neg><slot></slot></seq-op>`;
 export const SeqNegate = component('seq-negate')`<seq-op name="∼" pos><slot></slot></seq-op>`;
