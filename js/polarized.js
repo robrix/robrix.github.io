5cbef27e190b78058008a54c37a921c105004c7b
Rob Rix <rob.rix@me.com>
2021-08-19 00:04:42 -0400

    Define par and tensor.

--- js/polarized.js
+++ js/polarized.js
@@ -2,5 +2,7 @@ import { component } from './sequent.js';
 
 export const SeqWith = component('seq-with')`<seq-op name="&amp;" neg><slot></slot></seq-op>`;
 export const SeqSum = component('seq-sum')`<seq-op name="⊕" pos><slot></slot></seq-op>`;
+export const SeqPar = component('seq-par')`<seq-op name="⅋" neg><slot></slot></seq-op>`;
+export const SeqTensor = component('seq-tensor')`<seq-op name="⊗" pos><slot></slot></seq-op>`;
 export const SeqNot = component('seq-not')`<seq-op name="¬" neg><slot></slot></seq-op>`;
 export const SeqNegate = component('seq-negate')`<seq-op name="∼" pos><slot></slot></seq-op>`;
