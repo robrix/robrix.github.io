07a7ec2c6356f6a14b89b35fa4934f6f94ba7fb1
Rob Rix <rob.rix@me.com>
2021-08-19 00:08:25 -0400

    Define implication and coimplication.

--- js/polarized.js
+++ js/polarized.js
@@ -8,5 +8,7 @@ export const SeqPar = component('seq-par')`<seq-op name="⅋" neg><slot></slot><
 export const SeqTensor = component('seq-tensor')`<seq-op name="⊗" pos><slot></slot></seq-op>`;
 export const SeqBottom = component('seq-bottom')`<seq-op name="⊥" neg><slot></slot></seq-op>`;
 export const SeqOne = component('seq-one')`<seq-op name="1" pos><slot></slot></seq-op>`;
+export const SeqImplication = component('seq-impl')`<seq-op name="→" neg><slot></slot></seq-op>`;
+export const SeqCoimplication = component('seq-coimpl')`<seq-op name="⤚" pos><slot></slot></seq-op>`;
 export const SeqNot = component('seq-not')`<seq-op name="¬" neg><slot></slot></seq-op>`;
 export const SeqNegate = component('seq-negate')`<seq-op name="∼" pos><slot></slot></seq-op>`;
