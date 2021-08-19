635588e6e2d24079031686a6ccc22abdf91b7501
Rob Rix <rob.rix@me.com>
2021-08-19 08:03:43 -0400

    Translate more connectives, add E/R subscripts.

--- docs/js/polarized.js
+++ docs/js/polarized.js
@@ -6,8 +6,26 @@ export const SeqTop = component('seq-top')`<seq-op name="⊤" neg><slot></slot><
 export const SeqZero = component('seq-zero')`<seq-op name="0" pos><slot></slot></seq-op>`;
 export const SeqPar = component('seq-par')`<seq-op name="⅋" neg><slot></slot></seq-op>`;
 export const SeqTensor = component('seq-tensor')`<seq-op name="⊗" pos><slot></slot></seq-op>`;
-export const SeqBottom = component('seq-bottom')`<seq-op name="⊥" neg><slot></slot></seq-op>`;
-export const SeqOne = component('seq-one')`<seq-op name="1" pos><slot></slot></seq-op>`;
+export const SeqBottom = component('seq-bottom')`
+<style type="text/css">
+sub {
+  vertical-align: sub;
+  font-size: 60%;
+  line-height: 0;
+  color: var(--sequent-neg-colour);
+}
+</style><seq-op name="⊥" neg><sub slot="op-decoration" class="neg">R</sub><slot></slot></seq-op>
+`;
+export const SeqOne = component('seq-one')`
+<style type="text/css">
+sub {
+  vertical-align: sub;
+  font-size: 60%;
+  line-height: 0;
+  color: var(--sequent-pos-colour);
+}
+</style><seq-op name="1" pos><sub slot="op-decoration" class="pos">E</sub><slot></slot></seq-op>
+`;
 export const SeqImplication = component('seq-impl')`<seq-op name="→" neg><slot></slot></seq-op>`;
 export const SeqCoimplication = component('seq-coimpl')`<seq-op name="⤚" pos><slot></slot></seq-op>`;
 export const SeqNot = component('seq-not')`<seq-op name="¬" neg><slot></slot></seq-op>`;
