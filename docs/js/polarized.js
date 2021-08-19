57c36947a7ff353fecb40831b5cda61726ec2c48
Rob Rix <rob.rix@me.com>
2021-08-19 00:02:11 -0400

    Define negate.

--- docs/js/polarized.js
+++ docs/js/polarized.js
@@ -1,3 +1,4 @@
 import { component } from './sequent.js';
 
 export const SeqNot = component('seq-not')`<seq-op name="¬" neg><slot></slot></seq-op>`;
+export const SeqNegate = component('seq-negate')`<seq-op name="∼" pos><slot></slot></seq-op>`;
