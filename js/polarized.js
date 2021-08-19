0e8d3017a6e002524781a5cd69ce78a6857946cd
Rob Rix <rob.rix@me.com>
2021-08-19 00:02:39 -0400

    Define with.

--- js/polarized.js
+++ js/polarized.js
@@ -1,4 +1,5 @@
 import { component } from './sequent.js';
 
+export const SeqWith = component('seq-with')`<seq-op name="&amp;" neg><slot></slot></seq-op>`;
 export const SeqNot = component('seq-not')`<seq-op name="¬" neg><slot></slot></seq-op>`;
 export const SeqNegate = component('seq-negate')`<seq-op name="∼" pos><slot></slot></seq-op>`;
