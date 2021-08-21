de99882f55eb84cc0c7cc385dedec6985808632f
Rob Rix <rob.rix@me.com>
2021-08-21 04:59:31 -0400

    Use new-style inferences in the duality page.

--- js/polarized.js
+++ js/polarized.js
@@ -6,24 +6,8 @@ export const SeqTop = prefix('seq-top', '⊤', 'neg');
 export const SeqZero = prefix('seq-zero', '0', 'pos');
 export const SeqPar = infix('seq-par', '⅋', 'neg');
 export const SeqTensor = infix('seq-tensor', '⊗', 'pos');
-export const SeqBottom = prefix('seq-bottom', '⊥', 'neg', c => {
-  c.prototype.connectedCallback = function () {
-    const sub = document.createElement('sub');
-    sub.textContent = 'R';
-    sub.className = 'neg';
-    this.appendChild(sub);
-    sub.slot = 'op-decoration';
-  };
-});
-export const SeqOne = prefix('seq-one', '1', 'pos', c => {
-  c.prototype.connectedCallback = function () {
-    const sub = document.createElement('sub');
-    sub.textContent = 'E';
-    sub.className = 'pos';
-    this.appendChild(sub);
-    sub.slot = 'op-decoration';
-  };
-});
+export const SeqBottom = prefix('seq-bottom', '⊥', 'neg');
+export const SeqOne = prefix('seq-one', '1', 'pos');
 export const SeqImplication = infix('seq-impl', '→', 'neg');
 export const SeqCoimplication = infix('seq-coimpl', '⤚', 'pos');
 export const SeqNot = prefix('seq-not', '¬', 'neg');
